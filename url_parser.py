1c40bc2c9d66a9b201cd5800ba09e7342d8b3be9
Bauyrzhan Sovetkali <sovetkali@yandex-team.ru>
2020-12-18 21:21:31 +0300

    Removed function "print_parsed_url"

--- url_parser.py
+++ url_parser.py
@@ -5,16 +5,6 @@ import re
 
 protocols = ['https', 'http', 'ftp']
 
-def print_parsed_url(url, result_array):
-  print('URL: ' + url + '\n' + '**********')
-  summary = ''
-  summary += 'Protocol: ' + result_array[0] + '\n' + 'Host: ' + result_array[1] + '\n'
-  if(result_array[2] == '/'):
-    summary += 'Path: ' + result_array[3] + '\n'
-  elif(result_array[2] == '?'):
-    summary += 'Parameters: ' + result_array[3] + '\n'
-  return summary
-
 def parse_url(url):
   if isinstance(url, str):
     url = url.lower()
@@ -22,7 +12,14 @@ def parse_url(url):
       result = '^({0}:\/\/)([\S\w][^?\/]*)([\/|\?]?)([\S]*)'.format(protocol)
       if(re.search(result, url)):
         result_array = re.search(result, url).groups()
-        return print_parsed_url(url, result_array)
+        print('URL: ' + url + '\n' + '**********')
+        summary = ''
+        summary += 'Protocol: ' + result_array[0] + '\n' + 'Host: ' + result_array[1] + '\n'
+        if(result_array[2] == '/'):
+          summary += 'Path: ' + result_array[3] + '\n'
+        elif(result_array[2] == '?'):
+          summary += 'Parameters: ' + result_array[3] + '\n'
+        return summary
     return 'Undefined protocol'
   else:
     print('It\'s not URL. Enter URL')
