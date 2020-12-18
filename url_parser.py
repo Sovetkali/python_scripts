26747f9acfee7ba6ee09b6c792eefba4f71973c9
Bauyrzhan Sovetkali <sovetkali@yandex-team.ru>
2020-12-18 12:00:44 +0300

    Added functions parse_url() and print_parsed_url()

--- url_parser.py
+++ url_parser.py
@@ -1,6 +1,32 @@
 '''
 Script parses url like <host>,<domain>,<path>,[parameters]
 '''
+import re
 
-def parse_url(url, path = False, params = False):
-  pass
+protocols = ['https', 'http', 'ftp']
+
+def print_parsed_url(url, result_array):
+  print('URL: ' + url + '\n' + '**********')
+  summary = ''
+  summary += 'Protocol: ' + result_array[0] + '\n' + 'Host: ' + result_array[1] + '\n'
+  if(result_array[2] == '/'):
+    summary += 'Path: ' + result_array[3] + '\n'
+  elif(result_array[2] == '?'):
+    summary += 'Parameters: ' + result_array[3] + '\n'
+  return summary
+
+def parse_url(url):
+  if isinstance(url, str):
+    url = url.lower()
+    for protocol in protocols:
+      result = '^({0}:\/\/)([\S\w][^?\/]*)([\/|\?]?)([\S]*)'.format(protocol)
+      if(re.search(result, url)):
+        result_array = re.search(result, url).groups()
+        return print_parsed_url(url, result_array)
+    return 'Undefined protocol'
+  else:
+    print('It\'s not URL. Enter URL')
+
+
+print(parse_url('http://ya.ru?user=superuser&password=pass123'))
+print(parse_url('https://ya.ru/registration'))
